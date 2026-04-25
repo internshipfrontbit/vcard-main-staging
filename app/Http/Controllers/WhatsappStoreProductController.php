b878bae5206e6df59804950fbd7581fe323fd130
Meet Goyani <75927421+meetgoyani007-dev@users.noreply.github.com>
2026-04-25 14:37:28 +0530

    changes

--- app/Http/Controllers/WhatsappStoreProductController.php
+++ app/Http/Controllers/WhatsappStoreProductController.php
@@ -818,7 +818,7 @@ class WhatsappStoreProductController extends AppBaseController
 
                     if (!empty($mobileDiscountSettings)) {
                          foreach ($mobileDiscountSettings as $item) {
-                            if ($item['mobile'] == $userMobile) {
+                            if ($item['mobile'] == $request) {
 
                                 // If discount is 0 → no discount
                                 if ((float)$item['discount'] === 0.0) {
