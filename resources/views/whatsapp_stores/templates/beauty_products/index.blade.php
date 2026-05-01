ea2c1f97371d872ebadf23d3b5c9b090854e44e2
Meet Goyani <abhikatrodiya11@gmail.com>
2026-05-01 11:21:12 +0530

    changes

--- resources/views/whatsapp_stores/templates/beauty_products/index.blade.php
+++ resources/views/whatsapp_stores/templates/beauty_products/index.blade.php
@@ -1282,49 +1282,15 @@ object-fit: fill;
                 </style>
 
                 @php
-    $testimonials = [];
+                    $testimonials = [];
 
-    if (!empty($whatsappStore->testimonials)) {
-        $testimonials = json_decode($whatsappStore->testimonials, true);
-    }
-@endphp
-
-
-@if(!empty($testimonials) && count($testimonials) > 0)
-
-<section class="py-5 testimonial-section">
-  <div class="container">
-    <h2 class="text-center mb-4 mt-3 title-size" style="font-weight: 600;">What Our Clients Say</h2>
-    
-    <!-- Slick Slider Container -->
-    <div class="testimonial-slider">
-      
-      <!-- Slide 1 -->
-       @foreach($testimonials as $item)
-      <div class="px-2">
-        <div class="testimonial-card p-4 rounded-4 shadow-sm bg-white">
-          <div>
-            <svg class="quote-icon" viewBox="0 0 512 512">
-                <path d="M119.472,66.59C53.489,66.59,0,120.094,0,186.1c0,65.983,53.489,119.487,119.472,119.487c0,0-0.578,44.392-36.642,108.284c-4.006,12.802,3.135,26.435,15.945,30.418c9.089,2.859,18.653,0.08,24.829-6.389c82.925-90.7,115.385-197.448,115.385-251.8C238.989,120.094,185.501,66.59,119.472,66.59z"/>
-                <path d="M392.482,66.59c-65.983,0-119.472,53.505-119.472,119.51c0,65.983,53.489,119.487,119.472,119.487c0,0-0.578,44.392-36.642,108.284c-4.006,12.802,3.136,26.435,15.945,30.418c9.089,2.859,18.653,0.08,24.828-6.389C479.539,347.2,512,240.452,512,186.1C512,120.094,458.511,66.59,392.482,66.59z"/>
-            </svg>
-            <p class="review-text fst-italic">"{{ $item['review'] ?? '' }}"</p>
-          </div>
-          <div class="mt-3">
-            <div class="text-warning mb-2" style="font-size: 18px;">★★★★★</div>
-            <h6 class="mb-0 fw-bold">{{ $item['name'] ?? '' }}</h6>
-          </div>
-        </div>
-      </div>
+                    if (!empty($whatsappStore->testimonials)) {
+                        $testimonials = json_decode($whatsappStore->testimonials, true);
+                    }
+                @endphp
 
 
-      @endforeach
 
-    </div>
-  </div>
-</section>
-
-@endif
                 
 
                 
